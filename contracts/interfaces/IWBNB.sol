18bbd46bc47e8f2aa48515a39984da66f17eb994
Genta Sumantri <genta.sumantri@gmail.com>
2021-07-04 21:44:26 +0700

    change to external function

--- contracts/interfaces/IWBNB.sol
+++ contracts/interfaces/IWBNB.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.5.0;
 
 interface IWBNB {
-  function deposit() public payable;
+  function deposit() external payable;
 
-  function transfer(address to, uint256 value) public returns (bool);
+  function transfer(address to, uint256 value) external returns (bool);
 
-  function withdraw(uint256) public;
+  function withdraw(uint256) external;
 }
