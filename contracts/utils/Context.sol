605581e57f21963d2a15ddd99b47e556b1afda84
Genta Sumantri <genta.sumantri@gmail.com>
2021-07-17 10:37:23 +0700

    disable lint for empty constructors

--- contracts/utils/Context.sol
+++ contracts/utils/Context.sol
@@ -15,9 +15,9 @@ pragma solidity ^0.5.0;
 contract Context {
   // Empty internal constructor, to prevent people from mistakenly deploying
   // an instance of this contract, which should be used via inheritance.
-  constructor() internal {}
-
-  // solhint-disable-previous-line no-empty-blocks
+  constructor() internal {
+    // solhint-disable-previous-line no-empty-blocks
+  }
 
   function _msgSender() internal view returns (address payable) {
     return msg.sender;
