f4c4a4c7bd6ee009948c68926f90664449c2f613
Genta Sumantri <genta.sumantri@gmail.com>
2021-08-12 06:35:10 +0700

    add dotenv and gas-reporter

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,16 @@
 // hardhat.config.js
 
+require('dotenv').config();
 require('@nomiclabs/hardhat-waffle');
 require('@nomiclabs/hardhat-truffle5');
 require('solidity-coverage');
+require('hardhat-gas-reporter');
 
 const mochaOptions = require('./.mocharc.js');
 
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
   solidity: {
     version: '0.6.2',
@@ -17,4 +22,9 @@ module.exports = {
     },
   },
   mocha: mochaOptions,
+  gasReporter: {
+    enabled: process.env.ENABLE_GAS_REPORT,
+    currency: 'IDR',
+    coinmarketcap: process.env.API_KEY_CMC,
+  },
 };
